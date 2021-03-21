5ac170899811cf81bb916e478217c8fbf10530c6
AndreeaTr <andreea.traistaru1998@gmail.com>
2021-03-22 01:09:21 +0200

    2021

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Router, RouterLink } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-registration',
@@ -16,6 +17,8 @@ export class RegistrationComponent implements OnInit {
   loginError: boolean;
   isClicked: boolean;
   registrationForm: FormGroup;
+  registrationError: boolean;
+  errorList: any;
 
 
   ngOnInit() {
@@ -25,67 +28,74 @@ export class RegistrationComponent implements OnInit {
     // REACTIVE-FORM
     this.registrationForm = new FormGroup({
       'firstName': new FormControl('', {validators: [Validators.required]}),
-      'bday': new FormControl('', {validators: [Validators.required]}),
-      'email': new FormControl('', {validators: [Validators.required]}),
-      'pcode': new FormControl('', {validators: [Validators.required]}),
-      'password': new FormControl('', {validators: [Validators.required]}),
       'lastName': new FormControl('', {validators: [Validators.required]}),
+      'bday': new FormControl('', {validators: [Validators.required]}),
       'gender': new FormControl('', {validators: [Validators.required]}),
+      'email': new FormControl('', {validators: [Validators.required]}),
       'phnb': new FormControl('', {validators: [Validators.required]}),
+      'pcode': new FormControl('', {validators: [Validators.required]}),
       'country': new FormControl('', {validators: [Validators.required]}),
+      'password': new FormControl('', {validators: [Validators.required]}),
       'repeatPassword': new FormControl('', {validators: [Validators.required]})
     })
     //
   }
 
+
   register()
   {
-    console.log('Registration works!');
+    this.isClicked = true;
+
+    const headers = { 'Content-Type':  'application/json' };
+
+    let firstName = this.registrationForm.value["firstName"];
+    let lastName = this.registrationForm.value["lastName"];
+    let bday = this.registrationForm.value["bday"];
+    let gender = this.registrationForm.value["gender"];
+    let email = this.registrationForm.value["email"];
+    let phnb = this.registrationForm.value["phnb"];
+    let pcode = this.registrationForm.value["pcode"];
+    let country = this.registrationForm.value["country"];
+    let password = this.registrationForm.value["password"];
+    let matchingPassword = this.registrationForm.value["repeatPassword"];
+
+    const body = {
+
+      "firstname": firstName,
+      "lastname": lastName,
+      "dateofbirth": bday,
+      "gender": gender,
+      "email": email,
+      "phonenumber":phnb,
+      "postalcode":pcode,
+      "country":country,
+      "password": password,
+      "repeat_password": matchingPassword,
+
+    };
+    console.log(body);
+
+    this.http.post('http://127.0.0.1:5000/register', body, { 'headers': headers, 'observe': 'response' })
+      .subscribe(
+
+        responseBody =>
+          {
+            this.router.navigate(["/login"]);
+          },
+
+        err => //Intercepting forbidden status
+          {
+            this.registrationError = true;
+            this.isClicked = false;
+          },
+
+        () =>
+          {
+            console.log("ultimul");
+          }
+      )
+
   }
-
-  // get username() { return this.loginForm.get('username'); }
-  // get password() { return this.loginForm.get('password'); }
-  // get email() { return this.loginForm.get('email'); }
-  // get firstName() { return this.loginForm.get('firstName'); }
-  // get lastName() { return this.loginForm.get('lastName'); }
-
-  // login()
-  // {
-  //   this.isClicked = true;
-
-  //   const headers = { 'Content-Type':  'application/json' };
-
-  //   let username = this.loginForm.value["username"];
-  //   let password = this.loginForm.value["password"];
-
-  //   let acc = new Account(username,password);
-
-  //   this.http.post('https://virtual-library-system.herokuapp.com/authentication/login', JSON.stringify(acc), { 'headers': headers, 'observe': 'response' })
-  //     .subscribe(
-
-  //       responseBody =>
-  //         {
-  //           let lst = responseBody.body['jwt'].split(" ");;
-  //           let token = lst[1];
-  //           acc.setToken(token);
-  //           sessionStorage.setItem("token", acc.getToken());
-  //           sessionStorage.setItem("user", acc.getUsername());
-  //           this.router.navigate(["/search-results"]);
-  //         },
-
-  //       err => //Intercepting forbidden status
-  //         {
-  //           this.loginError = true;
-  //           this.isClicked = false;
-  //         },
-
-  //       () =>
-  //         {
-
-  //         }
-  //     );
-
-  // }
 
 }
 
